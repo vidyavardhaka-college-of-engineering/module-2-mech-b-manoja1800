bbbe886863e66a69b4067a47ae9764e91b361a43
manoja1800 <20me0048@vvce.ac.in>
2021-03-07 12:30:54 +0000

    yes

--- Program-10.c
+++ Program-10.c
@@ -6,10 +6,10 @@
 
 int main()
 {
-    int arr[50], n, i, small, large;
+    int arr[100], n, i, small, large;
     printf("Enter how many values you want to read and values");
     scanf("%d", &n);
-printf("enter the array:");
+printf("enter the value of a[%d] :i");
     for (i = 0; i < n; ++i)
     {
         //printf("Enter the value of a[%d] : ",i);
@@ -19,14 +19,14 @@ printf("enter the array:");
     small = arr[0];
     large = arr[0];
 
-    for (i = 1; i < n; ++i);
+    for (i = 1; i < n; i++);
     {
         if (arr[i] < small )//missing code
         {
            (small = arr[i]);//missing code
         }
         
-        if (arr[i] >large)//missing code
+        if (arr[i] >large);//missing code
         {
             large= arr[i];//missing code
         }
